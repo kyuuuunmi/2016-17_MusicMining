85b21726e68628d65fc1e8da8121c0914821f217
YUUUUNMI <kyuuuunmi@gmail.com>
2017-01-03 17:13:55 +0900

    playlists 수정

--- musicMining/routes/playlists.js
+++ musicMining/routes/playlists.js
@@ -47,10 +47,23 @@ function getMuiscPlaylist(req, res) {
             console.log("getConnection error" + error);
             res.status(500).send(error);
         } else {
+
             var sql_singer = 'select m.music_id, m.title, m.music_url, a.album_image_url, p.musician_name from play_list l join music m on l.music_id = m.music_id join album a on m.album_id = a.album_id join role r on m.music_id = r.music_id join musician p on r.musician_id = p.musician_id where l.user_id = ? and r.role_num= ?';
+            var sql_singer2 = 'select m.music_id, m.title, m.music_url, p.musician_name as musician_name, a.album_image_url from play_list l join music m on l.music_id = m.music_id join album a on m.album_id = a.album_id join role r on m.music_id = r.music_id join musician p on r.musician_id = p.musician_id where l.user_id = ? and r.role_num= ?'
             var sql_feat = 'select musician_name as featuring_musician_name  from role f join music m on f.music_id = m.music_id join musician fe on fe.musician_id = f.musician_id where f.music_id = ? and f.role_num = ?';
             var music_id;
-
+            connection.query(sql_singer2, [req.params.user_id, ROLE_SINGER], function(error, rows) {
+                if (error) {
+                    //callback(msg(1, err));
+                    res.status(500).send(error);
+                    console.log(error);
+                } else {
+                    music_id = rows[0].music_id;
+                    res.status(200).send(msg(0, rows));
+                    //callback(null, rows[0]);
+                }
+            });
+/*
             async.series([
                     function(callback) {
                         // 피처링 제외한 가수 탐색
@@ -91,7 +104,8 @@ function getMuiscPlaylist(req, res) {
                     }
 
                 });
-        }
+        */
+      }
     });
 }
 
