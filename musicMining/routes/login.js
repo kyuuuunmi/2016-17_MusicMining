1a12d7b0e4b97cce78ef8e25c1f0fa3afd364024
YUUUUNMI <kyuuuunmi@gmail.com>
2017-01-04 21:54:08 +0900

    비밀번호 찾기 기능 추가

--- musicMining/routes/login.js
+++ musicMining/routes/login.js
@@ -2,13 +2,15 @@ const express = require('express');
 const mysql = require('mysql');
 const async = require('async');
 const router = express.Router();
-const db_config = require('../config/db_config.json')
-const msg = require('../message.js')
+const db_config = require('../config/db_config.json');
+const msg = require('../message.js');
 const emailManager = require('../emailmanager.js');
+const passwdManager = require('../passwdmanager.js');
 const pool = mysql.createPool(db_config);
-//  var scrypt = require('scrypt');
-
 var checksum;
+
+//var scrypt = require('scrypt-js');
+//var scryptParameters = scrypt.params(0.1);
 
 router.route('/')
     .post(login)
@@ -16,6 +18,9 @@ router.route('/')
 
 router.route('/regist')
     .post(regist)
+
+router.route('/findpw')
+    .post(findPw)
 
 function login(req, res) {
     if (req.body.case == 0) {
@@ -40,15 +45,14 @@ function login(req, res) {
                                 callback(msg(1, {}));
                                 console.log('userCheckQuery err: ' + err);
                             } else
-                            if (rows.length < 1){
-                              console.log("rows length"+rows.length);
+                            if (rows.length < 1) {
+                                console.log("rows length" + rows.length);
                                 if (req.body.case == 0) callback(null, 'not user');
                                 else {
                                     callback(msg(1, {}));
                                     console.log('id or passwd is not correct');
                                 }
-                                }
-                            else {
+                            } else {
                                 callback(msg(0, {}));
                                 console.log('login');
                             }
@@ -80,7 +84,6 @@ function login(req, res) {
 function regist(req, res) {
     var userCheckQuery2 = 'select user_id from user where user_id = ?';
     var userCheckValue2 = [req.body.user_id];
-
     var eManager = new emailManager();
     var smtpTransporter = eManager.smtpTransporter();
     var mailOptions = eManager.mailOptions(req.body.user_id, req.body.passwd, req.body.name, req.body.gender, req.body.birth);
@@ -92,7 +95,7 @@ function regist(req, res) {
         } else {
             async.series([
                     function(callback) {
-                        conn.query(userCheckQuery2, passwdHashed, function(err, rows) {
+                        conn.query(userCheckQuery2, userCheckValue2, function(err, rows) {
                             if (err) {
                                 callback(msg(1, {}));
                                 console.log('userCheckQuery2 err: ' + err);
@@ -128,7 +131,8 @@ function regist(req, res) {
 
 function receiveMail(req, res) {
     var registUserQuery = 'insert into user(user_id, passwd, name, gender, birth) values(?, ?, ?, ?, ?)';
-    //var passwdHashed = scrypt.passwdHashed(userCheckValue2, 0.1);
+    //scrypt.hash.config.outputEncoding = "hex";
+    //var passwdHashed = scrypt.hashedPassword(req.query.passwd, 0.1);
     var registUserValue = [req.query.user_id, req.query.passwd, req.query.name, req.query.gender, req.query.birth];
     console.log(registUserValue);
     pool.getConnection(function(err, conn) {
@@ -143,7 +147,68 @@ function receiveMail(req, res) {
                 });
             } else res.status(500).send('<h2>회원가입 실패, 경로가 틀림</h2>');
         }
-    })
+    });
+}
+
+function findPw(req, res) {
+    var sql_getPw = 'select passwd from user where user_id = ?';
+    var val_userId = [req.body.user_id];
+    var eManager = new passwdManager();
+    var smtpTransporter = eManager.smtpTransporter();
+    var mailOptions;
+
+    pool.getConnection(function(err, conn) {
+        if (err) {
+            res.status(500).send(msg(1, 'Connection Error'));
+            console.log('db connection err: ' + err);
+        } else {
+            async.series([
+                function(callback) {
+                    // pw받아오기
+                    conn.query(sql_getPw, val_userId, function(err, rows) {
+                        if (err) {
+                            callback(msg(1, {}));
+                            console.log('sql_getPw err : ' + err);
+                        } else {
+                            if (rows.length < 1) {
+                                callback(msg(1, 'not user'));
+                            } else {
+                                user_pw = rows[0].passwd;
+                                mailOptions = eManager.mailOptions(req.body.user_id, user_pw);
+                                callback(null, 'user');
+                            }
+                        }
+                    });
+                },
+                function(callback) {
+                    smtpTransporter.sendMail(mailOptions, function(err, info) {
+                        if (err) {
+                            callback(msg(1,
+                                'Connection Err'
+                            ));
+                            console.log('sendMail err : ' + err);
+                        } else callback(null, 'sendMail success');
+
+                    });
+                    smtpTransporter.close();
+
+
+                }
+            ], function(err, result) {
+                if (err)
+                    if (err.err == 1) res.status(200).send(err);
+                    else res.status(500).send(err);
+                else {
+                    res.status(200).send(msg(0, {}));
+                    console.log(result);
+                }
+                conn.release();
+
+            });
+        }
+    });
+
+
 }
 
 module.exports = router;
